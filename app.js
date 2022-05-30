7e8c3ba113368326951199c45cba0bc45eb158aa
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-29 22:13:49 -0400

    Updated docs

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // Have to create a 16 x 16 grid of square divs 
+
+
+let container = document.getElementById("grid");
+
+var cell = document.createElement("div");
+cell.innerHTML = "TEXT";
+container.appendChild(cell);
