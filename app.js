5950db38746590ad1884463441abf175e92131cd
AndTorGuer <atorresguerrero9@gmail.com>
2022-06-05 16:07:23 -0400

    update

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-// Have to create a 16 x 16 grid of square divs 
+const container = document.getElementById('grid');
+
+function makeRows(rows,cols) {
+    // creates a custom CSS property by --customName
+    grid.style.setProperty('--grid-rows', rows);
+    grid.style.setProperty('--grid-cols',cols);
+    // creates the grid by looping
+    for(c = 0; c < (rows * cols); c++)
+    {
+        let cell = document.createElement('div');
+        cell.innerText = (c + 1);
+        grid.appendChild(cell).className = 'grid-item';
+    }; 
+};
+
+makeRows(16,16);
+
+function clearGrid(){
+    $(".grid").remove();
+};  
 
 
-let container = document.getElementById("grid");
 
-var cell = document.createElement("div");
-cell.innerHTML = "TEXT";
-container.appendChild(cell);
+
+
+
+
+
+
